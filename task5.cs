946ed6d06a4519653a72ee79c6a7d8f02cf6a6b2
Danil <danilivd2602@mail.ru>
2026-09-19 17:51:57 +0900

    задача 5

---
+++ task5.cs
@@ -0,0 +1,31 @@
+int[,] a = new int[3, 3];
+int[,] b = new int[3, 3];
+
+Console.WriteLine("введите матрицу: 3 строки по 3 числа через пробел");
+for (int i = 0; i < 3; i++)
+{
+    string line = Console.ReadLine() ?? "";
+    string[] parts = line.Split(' ');
+    for (int j = 0; j < 3; j++)
+    {
+        a[i, j] = Convert.ToInt32(parts[j]);
+    }
+}
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        b[j, i] = a[i, j];
+    }
+}
+
+Console.WriteLine("результат:");
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write(b[i, j] + " ");
+    }
+    Console.WriteLine();
+}
