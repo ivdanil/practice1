eadaa34e73295cfaaa7cad7ccf17d1dbfe94c04f
Danil <danilivd2602@mail.ru>
2026-09-19 17:50:24 +0900

    задача 4

---
+++ task4.cs
@@ -0,0 +1,21 @@
+double[] averages = new double[3];
+
+for (int student = 1; student <= 3; student++)
+{
+    Console.WriteLine($"оценки студента {student}:");
+    double sum = 0;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        Console.Write($"оценка {i}: ");
+        sum = sum + Convert.ToDouble(Console.ReadLine());
+    }
+
+    averages[student - 1] = sum / 5;
+}
+
+Console.WriteLine("средние оценки:");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"студент {i + 1}: {averages[i]:F2}");
+}
