744f54b7767eede84bca7d9ac961bcdbc6aaa2ea
Danil <danilivd2602@mail.ru>
2026-09-19 17:46:49 +0900

    задача 2

---
+++ task2.cs
@@ -0,0 +1,8 @@
+double rate = 80; 
+
+Console.Write("введите сумму в рублях: ");
+double rubles = Convert.ToDouble(Console.ReadLine());
+
+double dollars = rubles / rate;
+
+Console.WriteLine($"{rubles} руб. = {dollars:F2} $");
