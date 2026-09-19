986fdde449cb62b0b73b55c746fa10f81ab26b12
Danil <danilivd2602@mail.ru>
2026-09-19 17:38:05 +0900

    задача 1

---
+++ task1.cs
@@ -0,0 +1,16 @@
+// 1
+
+// Console.Write("введите длину: ");
+// double a = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("введите ширину: ");
+// double b = Convert.ToDouble(Console.ReadLine());
+
+// double area = a * b;
+// double perimeter = 2 * (a + b);
+
+// Console.WriteLine($"площадь: {area}");
+// Console.WriteLine($"периметр: {perimeter}");
+
+
+
